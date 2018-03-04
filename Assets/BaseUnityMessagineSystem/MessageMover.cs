a25080ecab97985bb3f7e7a03031b6737a4e1e19
James Gentry <18251756+palpitatertot@users.noreply.github.com>
2018-03-04 17:37:49 -0600

    garbage messaging system using findTag

---
+++ Assets/BaseUnityMessagineSystem/MessageMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MessageMover : MonoBehaviour {
+	NavMeshAgent agent;
+
+	private Vector3 target;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void MSGSetPosition(Vector3 position){
+		agent.destination = position;
+	}
+
+}
---
+++ Assets/BaseUnityMessagineSystem/Messenger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Messenger : MonoBehaviour {
+	GameObject[] listeners;
+
+	void Start() {
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			listeners = GameObject.FindGameObjectsWithTag("MessageReceiver");
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				foreach (GameObject o in listeners){
+					o.SendMessage ("MSGSetPosition", hit.point);
+				}
+			}
+		}
+	}
+}
